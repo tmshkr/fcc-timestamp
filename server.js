3c8fc939cb4f8f1faf3ef7a836916c85e229bd47
Glitch (tmshkr-fcc-timestamp) <none>
2019-02-17 19:56:12 +0000

    🏋🚟 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,58 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// parse timestamp
+app.get("/api/timestamp/:date_string?", function(req, res){
+  const input = req.params.date_string;
+  let date;
+  
+  if (input) {
+    if (/\D/.test(input)) {         // if input contains non-digit characters,
+      date = new Date(input);       // parse it as a date string
+    }
+    else {
+      const ms = parseInt(input);   // otherwise parse it as an integer in milliseconds
+      date = new Date(ms);
+    }
+  }
+  else {
+    date = new Date();              // if no input, return current time
+  }
+  
+  if (!date.valueOf()) {            // if the value of the date is falsy (NaN), it's an invalid date
+    res.json({"error" : "Invalid Date" });
+  }
+  else {
+    res.json({ "unix": date.getTime(), "utc" : date.toUTCString() });
+  }
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
